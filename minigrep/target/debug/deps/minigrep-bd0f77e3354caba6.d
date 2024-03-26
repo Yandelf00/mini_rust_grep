b058cbe52e60a34130d935a8187eaaa605b629bc
Yandelf00 <zakaria.afyf00@gmail.com>
2024-03-26 04:36:54 +0000

    add

---
+++ minigrep/target/debug/deps/minigrep-bd0f77e3354caba6.d
@@ -0,0 +1,5 @@
+/home/zakaria/rust_grep/mini_rust_grep/minigrep/target/debug/deps/minigrep-bd0f77e3354caba6: src/main.rs
+
+/home/zakaria/rust_grep/mini_rust_grep/minigrep/target/debug/deps/minigrep-bd0f77e3354caba6.d: src/main.rs
+
+src/main.rs:
---
+++ minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/home/zakaria/rust_grep/mini_rust_grep/minigrep/target/debug/minigrep: /home/zakaria/rust_grep/mini_rust_grep/minigrep/src/main.rs
